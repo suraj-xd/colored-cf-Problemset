9fc90d2dd7b184595ea9b6569684abfa34dda3ad
Suraj <unofficialartist3@gmail.com>
2022-08-16 14:42:22 +0530

    added another link

--- main.user.js
+++ main.user.js
@@ -5,6 +5,7 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://codeforces.com/problemset
+// @match        https://codeforces.com/problemset*
 // @match        https://codeforces.com/problemset/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=codeforces.com
 // @grant        none
